f0e0d3e01cf2c87c2729211e0b11c2ddcb58266c
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-09-01 22:45:40 +0530

    add container tools to all env

--- buildDocs.sh
+++ buildDocs.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-  # Check if pre-req commands exists
-  hash yq
-  hash antora
+# Check if pre-req commands exists
+hash yq
+hash antora
 
 _CURR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
 rm -rf ./gh-pages .cache
 
-yq w workshop.yaml -s workshop-attributes.yaml > workshop-site.yaml
+yq w workshop.yaml -s $_CURR_DIR/workshop-attributes.yaml > $_CURR_DIR/workshop-site.yaml
 
-antora --pull --stacktrace  workshop-site.yaml
+antora --pull --stacktrace  $_CURR_DIR/workshop-site.yaml
 
 open gh-pages/index.html
